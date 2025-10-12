4110eac0241fe40807072f2324308307fb387a45
Josh <josh.ocampo200529@gmail.com>
2025-10-12 21:17:06 +0800

    Type Conversion

--- Beginners/02 Primitives/06 Type Conversation/app.js
+++ Beginners/02 Primitives/06 Type Conversation/app.js
@@ -1,5 +1,13 @@
+// - Convert string to number
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount)
 
+//  - Convert number to string
+// money = money.toString();
+// money = String(money);
 
+// - Change string to decimal
 
 // - Convert string to number
 // amount = parseInt(amount);
@@ -11,3 +19,15 @@
 // money = String(money);
 
 // - Change string to decimal
+
+let amount = "150";
+let money = "25.20";
+
+console.log(typeof parseInt(amount));
+console.log(typeof +amount);
+console.log(typeof Number(amount));
+console.log(typeof parseFloat(money));
+
+console.log(typeof money.toString());
+console.log(typeof String(money));
+
