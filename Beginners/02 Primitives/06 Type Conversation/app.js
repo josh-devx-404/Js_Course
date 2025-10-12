aefcb4d7476ddaa4e47f8d7317bf3eb5ff4e85d9
Josh <josh.ocampo200529@gmail.com>
2025-10-12 20:27:19 +0800

    first commit

---
+++ Beginners/02 Primitives/06 Type Conversation/app.js
@@ -0,0 +1,13 @@
+
+
+
+// - Convert string to number
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount)
+
+//  - Convert number to string
+// money = money.toString();
+// money = String(money);
+
+// - Change string to decimal
