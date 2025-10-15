545d1aea3b775ce2691987669f429a8cfa0ae8d4
Josh <josh.ocampo200529@gmail.com>
2025-10-16 06:52:12 +0800

    Print your name & numbers from 0 through 1000 using for loop

--- Beginners/03 Control Flow/03 For Loop/app.js
+++ Beginners/03 Control Flow/03 For Loop/app.js
@@ -1,4 +1,4 @@
-// Print your name & numbers from 0 through 1000
+// Print your name & numbers from 0 through 1000 using for loop
 
     for (let i = 0; i <= 1000; i++) {
           console.log("josh-404", i);
