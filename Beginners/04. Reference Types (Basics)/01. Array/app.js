63e04569e3545809028d31b57725b8e4a2e47881
Josh-devx-404 <josh.ocampo200529@gmail.com>
2025-11-01 17:49:53 +0800

    Adds examples for array creation and item access
    Introduces sample arrays and demonstrates accessing individual
    elements using bracket notation to illustrate reference types
    for beginners.

--- Beginners/04. Reference Types (Basics)/01. Array/app.js
+++ Beginners/04. Reference Types (Basics)/01. Array/app.js
@@ -3,3 +3,15 @@
 // 3. Create array name (favNumbers) & store 4 fav Numbers.
 // 4. Create array name (mixedArr) store ["string", ["otherarray"], 123, true]
 // 5. Now Access each item in that array by using [] notation.
+
+let favSingers = ["Chris Brown", "Party Nextdoor", "Jacquees"];
+console.log(favSingers[1]);
+
+let favNumbers = [7,14,29,12];
+console.log(favNumbers[2]);
+
+let mixedArray = ["Josh", ["devx", "code"], 404, true];
+    console.log(mixedArray[0]);
+    console.log(mixedArray[1]);
+    console.log(mixedArray[2]);
+    console.log(mixedArray[3]);
