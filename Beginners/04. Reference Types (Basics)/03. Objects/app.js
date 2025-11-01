ded323d12eddcb6c9c578d0304ac11f3c5decd0d
Josh-devx-404 <josh.ocampo200529@gmail.com>
2025-11-01 17:33:18 +0800

    Completed

---
+++ Beginners/04. Reference Types (Basics)/03. Objects/app.js
@@ -0,0 +1,8 @@
+// 1. Create object name (car)
+// 2. add (type, model, color) as properties
+// 3. check the type of object
+// 4. Update the type property to "Toyota"
+// 5. Add new property wheels
+// 6. log car to the console.
+
+
