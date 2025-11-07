fd46f29fec1f4aa2e278e63732a4346a5aa949e1
Josh-devx-404 <josh.ocampo200529@gmail.com>
2025-11-07 11:16:28 +0800

    Function Declaration & Function Expression Completed

--- Beginners/05. Functions/02. Decleration & Expression/app.js
+++ Beginners/05. Functions/02. Decleration & Expression/app.js
@@ -10,7 +10,15 @@ greet("Josh-devx-404");
 const add = function(a, b){
     return a + b;
 };
+
 console.log(add(5, 10));
 
+// Function Expression with Default Parameter
+
+const greetings = function(name = "Guest"){
+    console.log(`Hello, ${name}! Welcome to our website.`);
+}
+
+greetings("Josh-devx-404");
 
 
